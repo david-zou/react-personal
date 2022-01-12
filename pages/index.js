46456f1f9aa21dcdacb05843bcb2b685bf25faab
David Zou <david.zou@outlook.com>
2022-01-11 16:13:48 -0800

    [feature] adding light/dark mode functionality

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,16 @@ import styles from '../styles/Home.module.css'
 
 import ContainerBlock from '../components/ContainerBlock'
 
+import { ThemeProvider } from "next-themes"; // enable light/dark mode
+
 export default function Home() {
   return (
-    <ContainerBlock
-      title="Welcome to David Zou's Portfolio Website"
-      description="David Zou's Portfolio Website"
-      >
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <ContainerBlock
+        title="Welcome to David Zou's Portfolio Website"
+        description="David Zou's Portfolio Website"
+        >
       </ContainerBlock>
+    </ThemeProvider>
   )
 }
