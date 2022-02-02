66bd7b026fdf8653c99fdeaba4ec493424e8e12a
David Zou <david.zou@outlook.com>
2022-02-02 06:16:01 -0800

    [feature] added dark mode compatbility to blogposts

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,22 @@
 import Navbar from '../components/Navbar'
 import BlogPost from '../components/BlogPost'
 
+// For theme state retrieval
+import { useTheme } from "next-themes"
+
+// Redux packages
+import { connect } from 'react-redux'
+import { setLightMode } from '../redux/actions/main'
+
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
-export default function Blog({ posts }) {
+function Blog({ posts }) {
+  // Needed to remember state on page refresh for redux lightMode state to work properly
+  const { theme } = useTheme();
+  setLightMode(theme)
+
   return (
     <div>
       <Navbar />
@@ -34,3 +45,13 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+const mapStateToProps = state => {
+  return { lightMode: state.main.lightMode }
+}
+
+const mapDispatchToProps = {
+  setLightMode
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blog)
